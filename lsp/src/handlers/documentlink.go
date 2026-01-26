74ee279db9a9ed9260a375d62a60a68a97b49b4a
Meng Lin <menglinmaker@gmail.com>
2026-01-26 23:27:24 +1100

    refactor to use findPathMatches

--- lsp/src/handlers/documentlink.go
+++ lsp/src/handlers/documentlink.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"log/slog"
+	"os"
 	"path-intellisense-lsp/src/glsp"
 	protocol "path-intellisense-lsp/src/protocol_3_16"
 	"path/filepath"
@@ -11,35 +12,30 @@ import (
 
 func TextDocumentDocumentLink(ctx *glsp.Context, params *protocol.DocumentLinkParams) ([]protocol.DocumentLink, error) {
 	slog.Debug(fmt.Sprintf("TextDocumentDocumentLink for file: %s", params.TextDocument.URI))
-	re := mustCompileLazyRegex(triggerCharacter + optionalPathPrefix + fmt.Sprintf("(/[^%s]+)+", illegalCharacters))
 
 	documentLinks := []protocol.DocumentLink{}
 	for i, line := range textLines(currentFiles[params.TextDocument.URI].Text) {
-		matches := re.FindAllStringIndex("\n"+line, -1)
-
-		for _, loc := range matches {
-			// By spec len(loc) == 2
-			start := loc[0]
-			end := loc[1] - 1
-			if start < 0 || end <= start || end > len(line) {
-				slog.Error(fmt.Sprintf("Failed to extract path from line:\n%s\nstart(%d), end(%d), len(%d)", line, start, end, len(line)))
-				continue
-			}
-
-			searchPath := line[start:end]
-			for _, absolutePath := range matchPath(searchPath, params.TextDocument.URI, "") {
+		for _, match := range findPathMatches(line) {
+			for _, absolutePath := range matchPath(match.Text, params.TextDocument.URI, "") {
 				target := "file://" + absolutePath
 				absoluteDir, _ := filepath.Split(params.TextDocument.URI[7:])
-				tooltip := strings.Replace(absolutePath, absoluteDir, "./", 1)
+
+				tooltip := "📄 File: "
+				fileInfo, err := os.Stat(absolutePath)
+				if err == nil && fileInfo.IsDir() {
+					tooltip = "📂 Folder: "
+				}
+				tooltip += strings.Replace(absolutePath, absoluteDir, "./", 1)
+
 				documentLinks = append(documentLinks, protocol.DocumentLink{
 					Range: protocol.Range{
 						Start: protocol.Position{
 							Line:      uint32(i),
-							Character: uint32(start),
+							Character: uint32(match.Start),
 						},
 						End: protocol.Position{
 							Line:      uint32(i),
-							Character: uint32(end),
+							Character: uint32(match.End),
 						},
 					},
 					Target:  &target,
@@ -48,6 +44,5 @@ func TextDocumentDocumentLink(ctx *glsp.Context, params *protocol.DocumentLinkPa
 			}
 		}
 	}
-
 	return documentLinks, nil
 }
