08227d29b744fc2b251707aa17c4dd1866fd2035
Meng Lin <menglinmaker@gmail.com>
2026-01-26 23:11:44 +1100

    refactor to display multiple links

--- lsp/src/handlers/documentlink.go
+++ lsp/src/handlers/documentlink.go
@@ -5,51 +5,47 @@ import (
 	"log/slog"
 	"path-intellisense-lsp/src/glsp"
 	protocol "path-intellisense-lsp/src/protocol_3_16"
+	"path/filepath"
+	"strings"
 )
 
 func TextDocumentDocumentLink(ctx *glsp.Context, params *protocol.DocumentLinkParams) ([]protocol.DocumentLink, error) {
 	slog.Debug(fmt.Sprintf("TextDocumentDocumentLink for file: %s", params.TextDocument.URI))
+	re := mustCompileLazyRegex(triggerCharacter + optionalPathPrefix + fmt.Sprintf("(/[^%s]+)+", illegalCharacters))
 
 	documentLinks := []protocol.DocumentLink{}
-	lines := textLines(currentFiles[params.TextDocument.URI].Text)
-	re := mustCompileLazyRegex(triggerCharacter + optionalPathPrefix + fmt.Sprintf("(/[^%s]+)*", illegalCharacters))
+	for i, line := range textLines(currentFiles[params.TextDocument.URI].Text) {
+		matches := re.FindAllStringIndex("\n"+line, -1)
 
-	for i, line := range lines {
-		search := "\n" + line
-		matches := re.FindAllStringIndex(search, -1)
-		for _, match := range matches {
-			if len(match) < 2 {
-				continue
-			}
-			start := match[0]
-			end := match[1] - 1
+		for _, loc := range matches {
+			// By spec len(loc) == 2
+			start := loc[0]
+			end := loc[1] - 1
 			if start < 0 || end <= start || end > len(line) {
-				continue
-			}
-			pathText := search[start:match[1]][1:]
-			if pathText == "" {
+				slog.Error(fmt.Sprintf("Failed to extract path from line:\n%s\nstart(%d), end(%d), len(%d)", line, start, end, len(line)))
 				continue
 			}
 
-			matchedPaths := matchPath(pathText, string(params.TextDocument.URI), "")
-			if len(matchedPaths) == 0 {
-				continue
+			searchPath := line[start:end]
+			for _, absolutePath := range matchPath(searchPath, params.TextDocument.URI, "") {
+				target := "file://" + absolutePath
+				absoluteDir, _ := filepath.Split(params.TextDocument.URI[7:])
+				tooltip := strings.Replace(absolutePath, absoluteDir, "./", 1)
+				documentLinks = append(documentLinks, protocol.DocumentLink{
+					Range: protocol.Range{
+						Start: protocol.Position{
+							Line:      uint32(i),
+							Character: uint32(start),
+						},
+						End: protocol.Position{
+							Line:      uint32(i),
+							Character: uint32(end),
+						},
+					},
+					Target:  &target,
+					Tooltip: &tooltip,
+				})
 			}
-
-			target := protocol.DocumentUri("file://" + matchedPaths[0])
-			documentLinks = append(documentLinks, protocol.DocumentLink{
-				Range: protocol.Range{
-					Start: protocol.Position{
-						Line:      uint32(i),
-						Character: uint32(start),
-					},
-					End: protocol.Position{
-						Line:      uint32(i),
-						Character: uint32(end),
-					},
-				},
-				Target: &target,
-			})
 		}
 	}
 
